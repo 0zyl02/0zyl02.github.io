816894021c4ac1c501b32aba2f6041b8dd74b1a0
0zyl02 <1147894228@qq.com>
2020-05-16 10:52:27 +0800

    update

--- js/node.js
+++ js/node.js
@@ -29,5 +29,5 @@ app.post('/file_upload',function (req, res) {
 var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log("应用实例，访问地址为 http://", host, port)
+    console.log("应用实例，访问地址为 https://", host, port)
 })
